ef04f48d679a68a99c10714d76afa31838fe5a11
elvis <1255177148@qq.com>
2022-11-08 13:43:49 +0800

    shiro框架--可以打成war包，放在外部tomcat下运行

--- src/main/java/com/hezhan/shirodemo/ShiroDemoApplication.java
+++ src/main/java/com/hezhan/shirodemo/ShiroDemoApplication.java
@@ -3,13 +3,44 @@ package com.hezhan.shirodemo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @MapperScan("com.hezhan.shirodemo.mapper")
-public class ShiroDemoApplication {
+public class ShiroDemoApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return super.configure(builder);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ShiroDemoApplication.class, args);
+    }
+
+    /**
+     * 跨域过滤器
+     * @return  @EnableTransactionManagement
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration corsConfiguration = new CorsConfiguration();
+        /*是否允许请求带有验证信息*/
+        corsConfiguration.setAllowCredentials(true);
+        /*允许访问的客户端域名*/
+        corsConfiguration.addAllowedOrigin("*");
+        /*允许服务端访问的客户端请求头*/
+        corsConfiguration.addAllowedHeader("*");
+        /*允许访问的方法名,GET POST等*/
+        corsConfiguration.addAllowedMethod("*");
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
     }
 
 }
